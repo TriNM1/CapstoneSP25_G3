aae1b802b014e6ec259dcdee5d3c17443551ea37
Lê Quốc Khánh <LQKhanh0@gmail.com>
2025-02-12 00:33:16 +0700

    user manage account

--- ToySharing/ToySharingAPI/Controllers/UserController.cs
+++ ToySharing/ToySharingAPI/Controllers/UserController.cs
@@ -15,6 +15,48 @@ namespace ToySharingAPI.Controllers
         {
             _context = context;
         }
-        
+        //Manage account
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDTO>> GetUserById(int id)
+        {
+            var user = await _context.Users
+                .Where(u => u.UserId == id)
+                .Select(u => new DTO.UserDTO
+                {
+                    UserId = u.UserId,
+                    Name = u.Name,
+                    Email = u.Email,
+                    Phone = u.Phone,
+                    Address = u.Address,
+                    Status = u.Status,
+                    Avatar = u.Avatar,
+                    Rating = u.Rating
+                })
+                .FirstOrDefaultAsync();
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+        //Edit account
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserDTO userDto)
+        {
+            var existingUser = await _context.Users.FindAsync(id);
+            if (existingUser == null)
+            {
+                return NotFound();
+            }
+            existingUser.Name = userDto.Name;
+            existingUser.Email = userDto.Email;
+            existingUser.Phone = userDto.Phone;
+            existingUser.Address = userDto.Address;
+            existingUser.Status = userDto.Status;
+            existingUser.Avatar = userDto.Avatar;
+
+            await _context.SaveChangesAsync();
+            return Ok(); 
+        }
     }
 }
