9335a8fdc4ef50072cea025a371b98eff182124a
lqkhanhno <LQKhanh0@gmail.com>
2025-02-25 19:02:58 +0700

    save
    save

--- ToySharing/ToySharingAPI/Models/Image.cs
+++ ToySharing/ToySharingAPI/Models/Image.cs
@@ -7,11 +7,11 @@ public partial class Image
 {
     public int ImageId { get; set; }
 
-    public int ProductId { get; set; }
+    public int? ProductId { get; set; }
 
-    public string Path { get; set; } = null!;
+    public string? Path { get; set; }
 
     public DateTime? CreateTime { get; set; }
 
-    public virtual Product Product { get; set; } = null!;
+    public virtual Product? Product { get; set; }
 }
