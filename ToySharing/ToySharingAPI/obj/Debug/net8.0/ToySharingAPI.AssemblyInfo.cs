dc8589ff8e2ed5b0039f0c508449ec068336dc69
AkamexBii <trungtalon31@gmail.com>
2025-03-08 09:59:28 +0700

    Update User log

--- ToySharing/ToySharingAPI/obj/Debug/net8.0/ToySharingAPI.AssemblyInfo.cs
+++ ToySharing/ToySharingAPI/obj/Debug/net8.0/ToySharingAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToySharingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10b2edd29ff76de9194e277781d7b5eef5a269c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee322f19e7b15d28aaa715f727eb2e66eba00f04")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToySharingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToySharingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
