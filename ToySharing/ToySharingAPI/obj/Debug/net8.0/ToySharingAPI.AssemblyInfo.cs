0d1bf007d047533e5a62bdf4ad5501822842518d
lqkhanhno <LQKhanh0@gmail.com>
2025-03-18 23:07:33 +0700

    commit

--- ToySharing/ToySharingAPI/obj/Debug/net8.0/ToySharingAPI.AssemblyInfo.cs
+++ ToySharing/ToySharingAPI/obj/Debug/net8.0/ToySharingAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToySharingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+906f9a10e82f8eade8f7bcca15dbe2f78aba3f93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7f7887fdd4d51f32cb944bbefae2e0d849e969c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToySharingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToySharingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
